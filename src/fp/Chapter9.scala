172c0977527125ff75d590d49e36b4de56cefa98
Ivan Valchev <ivalchev.work@gmail.com>
2015-05-31 23:49:35 +0300

    changes in Chapter9

--- src/fp/Chapter9.scala
+++ src/fp/Chapter9.scala
@@ -1,5 +1,7 @@
 package fp
 
+import fp.chapter4.Left
+
 /**
  * Created by ivan on 5/27/15.
  */
@@ -8,8 +10,9 @@ trait Parsers[ParseError, Parser[+ _]] {
   self =>
   def run[A](p: Parser[A])(input: String): Either[ParseError, A]
 
-  def char(c: Char): Parser[Char]
+  def map[A, B](p: Parser[A])(f: A => B): Parser[B]
 
+  def char(c: Char): Parser[Char]
 
   def or[A](p1: Parser[A], p2: Parser[A]): Parser[A]
 
@@ -18,6 +21,12 @@ trait Parsers[ParseError, Parser[+ _]] {
   implicit def parserOps[A](p: Parser[A]): ParserOps[A] = ParserOps(p)
 
   implicit def asStringParser[A](a: A)(implicit f: A => Parser[String]): ParserOps[String] = ParserOps(f(a))
+
+  def many[A](a: A): Parser[Int]
+
+  def many1[A](a: A)(notFound: ParseError) = map(many(a))(i => if (i == 0) Left(notFound) else Some(i))
+
+  def then[A, B](a: A, b: B): Parser[(Int, Int)]
 
   case class ParserOps[A](p: Parser[A]) {
     def |[B >: A](p2: Parser[B]) = self.or(p, p2)
--- src/fp/Pascal.scala
+++ src/fp/Pascal.scala
@@ -1,8 +1,21 @@
 package fp
 
+import fp.Chapter6.State
+
 /**
  * Created by ivan on 5/31/15.
  */
-object Pascal {
+object Pascal extends App {
 
+  def pTriangleOf(size: Int) = {
+    def s(n: Int): State[List[Int], List[List[Int]]] = {
+      State.sequence(List.fill(n)(State(l => {
+        val nl = (1 :: (l zip l.tail map { case (i, j) => i + j })) :+ 1
+        (nl, nl)
+      })))
+    }
+    s(size).run(List(1))._1.map(_.map(_.toString).reduce(_ + " " + _))
+  }
+
+  pTriangleOf(60*4).foreach(println)
 }
