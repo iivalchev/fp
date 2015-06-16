580201bf5fb2df3b04b816538cf1870e1b66c21a
Ivan Valchev <ivan.valchev@estafet.com>
2015-06-16 14:24:42 +0300

    Chapter9

--- src/fp/Chapter9.scala
+++ src/fp/Chapter9.scala
@@ -12,28 +12,98 @@ import scala.util.matching.Regex
 
 object Chapter9 {
 
-  trait Parsers[ParseError, Parser[+ _]] {
+  object ReferenceTypes {
+    type Parser[+A] = Location => Result[A]
+
+    trait Result[+A]
+
+    case class Success[+A](get: A, charsConsumed: Int) extends Result[A]
+
+    case class Failure[+A](get: ParseError) extends Result[A]
+
+
+    object Reference extends Parsers[Parser] {
+      override def run[A](p: Parser[A])(input: String): Either[ParseError, A] = p(Location(input, 0)) match {
+        case Success(a, _) => Right(a)
+        case Failure(e) => Left(e)
+      }
+
+      override def flatMap[A, B](p: Parser[A])(f: (A) => Parser[B]): Parser[B] = ???
+
+      override def or[A](p1: Parser[A], p2: => Parser[A]): Parser[A] = ???
+
+      override implicit def string(s: String): Parser[String] = {
+        in => {
+          if (in.input.startsWith(s, in.offset)) {
+            Success(s, s.length)
+          } else {
+            Failure(Location(in.input.substring(in.offset)).toError("Expected: " + s))
+          }
+        }
+      }
+
+      override def scope[A](msg: String)(p: Parser[A]): Parser[A] = ???
+
+      override implicit def regex(r: Regex): Parser[String] = {
+        loc =>
+          r.findPrefixOf(loc.input.substring(loc.offset)) match {
+            case Some(m) => Success(m, m.length)
+            case _ => Failure(ParseError(List((loc, "regex " + r))))
+          }
+      }
+
+      override def slice[A](p: Parser[A]): Parser[String] = {
+        loc => {
+          p(loc) match {
+            case Success(_, charsConsumed) => Success(loc.input.substring(loc.offset, loc.offset + charsConsumed), charsConsumed)
+            case Failure(e) => Failure(e)
+          }
+        }
+      }
+
+      override def fail: Parser[Nothing] = ???
+
+      override def label[A](msg: String)(p: Parser[A]): Parser[A] = ???
+
+      override def attempt[A](p: Parser[A]): Parser[A] = ???
+    }
+
+  }
+
+  trait Parsers[Parser[+ _]] {
     self =>
 
     def run[A](p: Parser[A])(input: String): Either[ParseError, A]
+
+
+    def flatMap[A, B](p: Parser[A])(f: A => Parser[B]): Parser[B]
+
+    def or[A](p1: Parser[A], p2: => Parser[A]): Parser[A]
+
+    implicit def string(s: String): Parser[String]
+
+    implicit def regex(r: Regex): Parser[String]
+
+    def slice[A](p: Parser[A]): Parser[String]
+
+    def attempt[A](p: Parser[A]): Parser[A]
+
+    def label[A](msg: String)(p: Parser[A]): Parser[A]
+
+    def scope[A](msg: String)(p: Parser[A]): Parser[A]
+
+
+    def fail: Parser[Nothing]
 
     def succeeded[A](a: A): Parser[A] = string("").map(_ => a)
 
     def char(c: Char): Parser[Char] = string(c.toString).map(_.charAt(0))
 
-    def flatMap[A, B](p: Parser[A])(f: A => Parser[B]): Parser[B]
-
     def map[A, B](p: Parser[A])(f: A => B): Parser[B] = flatMap(p)(a => succeeded(f(a)))
 
     def map2[A, B, C](p1: Parser[A], p2: => Parser[B])(f: (A, B) => C): Parser[C] = flatMap(p1)(a => map(p2)(b => f(a, b)))
 
-    def or[A](p1: Parser[A], p2: => Parser[A]): Parser[A]
-
     def listOfN[A](n: Int, p: Parser[A]): Parser[List[A]] = if (n == 0) succeeded(List()) else map2(p, listOfN(n - 1, p))(_ :: _)
-
-    implicit def string(s: String): Parser[String]
-
-    implicit def regex(r: Regex): Parser[String]
 
     implicit def parserOps[A](p: Parser[A]): ParserOps[A] = ParserOps(p)
 
@@ -43,13 +113,17 @@ object Chapter9 {
 
     def many1[A](p: Parser[A])(notFound: ParseError) = map2(p, p.many)
 
-    def slice[A](p: Parser[A]): Parser[String]
-
     def product[A, B](p1: Parser[A], p2: => Parser[B]): Parser[(A, B)] = map2(p1, p2)((_, _))
 
     def op1[A, B](p: Parser[(A, B)]): Parser[A] = p.map(_._1)
 
     def op2[A, B](p: Parser[(A, B)]): Parser[B] = p.map(_._2)
+
+
+    def errorLocation(e: ParseError): Location = e.stack.head._1
+
+    def errorMessage(e: ParseError): String = e.stack.head._2
+
 
     case class ParserOps[A](p: Parser[A]) {
       def |[B >: A](p2: Parser[B]) = self.or(p, p2)
@@ -100,8 +174,26 @@ object Chapter9 {
             case _ => false
           }
         }
+
+      def attemptLaw = Prop.check(run(attempt(string("a").flatMap(_ => fail)) | string("b"))("b") == Right("a")) &&
+        Prop.check(run(string("a").flatMap(_ => fail) | string("b"))("b") match {
+          case _: Left => true
+          case _ => false
+        })
     }
 
+  }
+
+  case class ParseError(stack: List[(Location, String)])
+
+  case class Location(input: String, offset: Int = 0) {
+    lazy val line = input.slice(0, offset + 1).count(_ == "/n") + 1
+    lazy val col = input.lastIndexOf("/n") match {
+      case -1 => offset + 1
+      case lineStart => offset - lineStart
+    }
+
+    def toError(msg: String) = ParseError(List((this, msg)))
   }
 
   trait JSON
@@ -118,7 +210,7 @@ object Chapter9 {
 
   case class JObject(get: Map[String, JSON]) extends JSON
 
-  def jsonParser[ParseError, Parser[+ _]](P: Parsers[ParseError, Parser]): Parser[JSON] = {
+  def jsonParser[ParseError, Parser[+ _]](P: Parsers[Parser]): Parser[JSON] = {
     import P._
 
     def NAME: Parser[String] = regex( """"(.*)"""".r)
