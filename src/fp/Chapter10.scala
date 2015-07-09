ff3d8b84caac527d9f61117b99632951125258de
Ivan Valchev <ivan.valchev@estafet.com>
2015-07-09 10:32:47 +0300

    Chapter 10

--- src/fp/Chapter10.scala
+++ src/fp/Chapter10.scala
@@ -1,5 +1,9 @@
 package fp
 
+import fp.Chapter7.Par
+
+import scala.Predef.augmentString
+
 /**
  * Created by Ivan Valchev (ivan.valchev@estafet.com) on 6/24/15.
  */
@@ -43,15 +47,68 @@ object Chapter10 extends App {
     def op(a1: (A) => A, a2: (A) => A): (A) => A = a1 compose a2
     def zero: (A) => A = a => a
   }
+  def dual[A](m: Monoid[A]) = new Monoid[A] {
+    def op(a1: A, a2: A): A = m.op(a2, a1)
+    def zero: A = m.zero
+  }
   def concatenate[A](as: List[A])(m: Monoid[A]): A = {
     as.foldLeft(m.zero)(m.op)
   }
   def foldMap[A, B](as: List[A], m: Monoid[B])(f: A => B): B = {
     as.foldLeft(m.zero)((b, a) => m.op(b, f(a)))
   }
-//  def foldLeft[A, B](as: List[A], zero: B)(f: (B, A) => B): B = {
-//    foldMap(as, )
-//  }
+  def foldLeft[A, B](as: List[A], zero: B)(f: (B, A) => B): B = {
+    foldMap(as, dual(endoMonoid[B]))(a => b => f(b, a))(zero)
+  }
+  def foldRight[A, B](as: List[A], zero: B)(f: (A, B) => B): B = {
+    foldMap(as, endoMonoid[B])(f.curried)(zero)
+  }
+  def foldMapV[A, B](v: IndexedSeq[A], m: Monoid[B])(f: A => B): B = {
+    if (v.isEmpty) m.zero
+    else if (v.size == 1) f(v.head)
+    else {
+      val (l, r) = v.splitAt(v.size / 2)
+      m.op(foldMapV(l, m)(f), foldMapV(r, m)(f))
+    }
+  }
+  def par[A](m: Monoid[A]): Monoid[Par[A]] = new Monoid[Par[A]] {
+    def op(a1: Par[A], a2: Par[A]): Par[A] = Par.map2(a1, a2)(m.op)
+    def zero: Par[A] = Par.unit(m.zero)
+  }
+
+  //  def parFoldMap[A, B](v: IndexedSeq[A], m: Monoid[B])(f: A => B): Par[B] = {
+  //    Par.flatMap(Par.parMap(v)(f))(bs => foldMapV(bs, par(m))(b => Par.unit(b)))
+  //  }
+
+  sealed trait WC
+
+  case class Stub(chars: String) extends WC
+
+  case class Part(lStub: String, words: Int, rStub: String) extends WC
+
+  val wcMonoid: Monoid[WC] = new Monoid[WC] {
+    def op(a1: WC, a2: WC): WC = (a1, a2) match {
+      case (Stub(c1), Stub(c2)) => Stub(c1 + c2)
+      case (Stub(c), Part(l, w, r)) => Part(c + l, w, r)
+      case (Part(l, w, r), Stub(c)) => Part(l, w, r + c)
+      case (Part(l1, w1, r1), Part(l2, w2, r2)) => Part(l1, w1 + (if ((r1 + l2).isEmpty) 0 else 1) + w2, r2)
+    }
+    def zero: WC = Stub("")
+  }
+
+  def count(s: String): Int = {
+    def unstub(s: String) = s.length min 1
+    foldMapV(s.toIndexedSeq, wcMonoid)(c => {
+      if (c.isWhitespace) {
+        Part("", 0, "")
+      } else {
+        Stub(c.toString)
+      }
+    }) match {
+      case Stub(s) => unstub(s)
+      case Part(l, w, r) => unstub(l) + w + unstub(r)
+    }
+  }
 
   val words = List("Hic", "Est", "Index")
   println(words.foldRight(stringMonoid.zero)(stringMonoid.op))
