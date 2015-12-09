306c8be255a14f99e0ca97e5278524ceb9351bff
Ivan Valchev <ivan.valchev@estafet.com>
2015-12-09 09:24:15 +0000

    DFS

--- src/fp/DFS.scala
+++ src/fp/DFS.scala
@@ -10,24 +10,28 @@ object DFS extends App {
 
   case class Node[A](value: A, adjecent: () => List[Node[A]])
 
-  def directedCycle[A](node: Node[A]): List[Node[A]] = {
+  def directedCycle[A](node: Node[A]): List[List[Node[A]]] = {
     @tailrec
-    def dfs(visited: Set[Node[A]], stack: List[Node[A]], path: List[Node[A]]): List[Node[A]] = {
-      stack match {
-        case h :: _ if visited(h) => h :: path
-        case h :: t => dfs(visited + h, h.adjecent() ::: t, h :: path)
-        case _ => Nil
+    def backTrack(n: Node[A], fullPath: List[Node[A]], path: List[Node[A]]): List[Node[A]] = {
+      fullPath match {
+        case h :: _ if h == n => (h :: path).reverse
+        case h :: t => backTrack(n, t, h :: path)
       }
     }
-    dfs(Set(), List(node), List())
+    @tailrec
+    def dfs(visited: Set[Node[A]], stack: List[Node[A]], path: List[Node[A]], cycles: List[List[Node[A]]]): List[List[Node[A]]] = {
+      stack match {
+        case h :: t if visited(h) => dfs(visited, t, path, (h :: backTrack(h, path, List())) :: cycles)
+        case h :: t => dfs(visited + h, h.adjecent() ::: t, h :: path, cycles)
+        case _ => cycles
+      }
+    }
+    dfs(Set(), List(node), List(), List())
   }
 
-
-  assert(directedCycle(Node(1, () => List())).isEmpty)
-
-  val node: Node[Int] = Node(1, () => List(node))
-  private val cycle: List[Node[Int]] = directedCycle(node)
+  val node2: Node[Int] = Node(2, () => List(Node(3, () => List(Node(4, () => List(node2))))))
+  val node: Node[Int] = Node(1, () => List(node, node2))
+  private val cycle: List[List[Node[Int]]] = directedCycle(node)
   println(cycle)
-  assert(cycle.nonEmpty)
 
 }
